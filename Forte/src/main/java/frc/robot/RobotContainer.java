473cde9f33caea9f39050177c4e79e115e399e2c
AryanKammili <140930533+AryanKammili@users.noreply.github.com>
2024-03-23 11:44:15 -0500

    Shooter Added

--- Forte/src/main/java/frc/robot/RobotContainer.java
+++ Forte/src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
@@ -12,7 +15,13 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 import frc.robot.commands.DriverIntakeFeedback;
+import frc.robot.subsystems.Drive.Drive;
+import frc.robot.subsystems.Drive.GyroIO;
+import frc.robot.subsystems.Drive.GyroIOPigeon2;
+import frc.robot.subsystems.Drive.ModuleIOSim;
+import frc.robot.subsystems.Drive.ModuleIOSparkMax;
 import frc.robot.subsystems.IndexerIntake.IndexerIntake;
+import frc.robot.subsystems.Shooter.Shooter;
 
   
 /**
@@ -48,8 +57,11 @@ public class RobotContainer {
         operator.rightTrigger().onTrue(new InstantCommand(() -> IndexerIntake.smartFeed()));
         operator.rightTrigger().onFalse(new InstantCommand(() -> IndexerIntake.stopFeed()));
         operator.rightTrigger().whileTrue(new DriverIntakeFeedback(IndexerIntake, driver, operator));
+        
+        
         //opX.onTrue(s_Feeder.setShooterToSpeaker());
     }
+
 
     public CommandXboxController getDriveController(){
         return driver;
---
+++ Forte/src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.Shooter;
+
+/** Add your docs here. */
+public class ShooterConstants {
+    public static final Integer UPPER_ROLLER_PORT = 34;
+    public static final Integer LOWER_ROLLER_PORT = 35;
+    public static final Integer PIVOT_PORT = 32;
+    public static final Integer PIVOT_ENCODER_PORT = 0;
+
+    public static final Double PIVOT_OFFSET = 0.656761d;
+
+    public static final Double PIVOT_P = 3.75d;
+    public static final Double PIVOT_I = 0d;
+    public static final Double PIVOT_D = 0.35d;
+
+    public static final Double ROLLER_P = 10d;
+    public static final Double ROLLER_I = 0d;
+    public static final Double ROLLER_D = 0.001d;
+}
